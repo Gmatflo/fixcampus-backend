71a49927990cb276cfb9b38ae2fcfca347f6fedf
Mauricio1608 <u202419277@upc.edu.pe>
2026-09-24 09:54:57 -0500

    Add custom query methods to RecomendacionRepository

--- src/main/java/pe/edu/upc/fixcampus/fixcampus/repositories/RecomendacionRepository.java
+++ src/main/java/pe/edu/upc/fixcampus/fixcampus/repositories/RecomendacionRepository.java
@@ -2,7 +2,16 @@ package pe.edu.upc.fixcampus.fixcampus.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.upc.fixcampus.fixcampus.entities.Recomendacion;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecomendacionRepository extends JpaRepository<Recomendacion, Long> {
     boolean existsByReporte_IdReporte(Long idReporte);
+    
+    List<Recomendacion> findByPrioridadSugeridaIgnoreCase(String prioridadSugerida);
+
+
+    @Query("select rec from Recomendacion rec join rec.reporte r join r.categoria c " +
+            "where lower(c.nombre) = lower(:nombreCategoria)")
+    List<Recomendacion> findByCategoriaDelReporte(@Param("nombreCategoria") String nombreCategoria);
 }
